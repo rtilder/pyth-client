b19abc09aa311af2070219aa413ae2c0ec5fd888
Ryan Tilder <ryan@tilder.org>
2021-10-14 14:37:45 -1000

    Tidy up the build configs

--- os-x-arm.cmake
+++ os-x-arm.cmake
@@ -1,5 +1,3 @@
-# gcc compiler/linker flags
-add_compile_options( -ggdb -Wall -Wextra -Werror -m64 -I/opt/homebrew/openssl/include/ -I/opt/homebrew/zstd/include)
-set( CMAKE_CXX_FLAGS -std=c++11 )
-set( CMAKE_C_FLAGS -std=c99 )
-set( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pthread -L/opt/homebrew/openssl/lib -L/opt/homebrew/lib")
+# toolchain/platform specific options
+add_compile_options(-I/opt/homebrew/opt/openssl/include/ -I/opt/homebrew/include)
+set( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pthread -L/opt/homebrew/opt/openssl/lib -L/opt/homebrew/lib")
--- os-x-x86.cmake
+++ os-x-x86.cmake
@@ -1,5 +1,3 @@
-# gcc compiler/linker flags
-add_compile_options( -ggdb -Wall -Wextra -Werror -m64 -I/usr/local/opt/openssl/include/ -I/usr/local/opt/zstd/include)
-set( CMAKE_CXX_FLAGS -std=c++11 )
-set( CMAKE_C_FLAGS -std=c99 )
+# toolchain/platform specific options
+add_compile_options( -ggdb -Wall -Wextra -Werror -m64 -I/usr/local/opt/openssl/include/ -I/usr/local/include)
 set( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pthread -L/usr/local/opt/openssl/lib -L/usr/local/lib")
